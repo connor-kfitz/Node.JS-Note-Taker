1f2965871415e9c66d89fc5219fffba25ee8ea81
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-17 18:13:44 -0400

    Re-sub Final Commit before Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
